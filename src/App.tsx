690971c80e65a1ac4f49f56317a23df9c5498779
jcandioto <jcandioto8911@gmail.com>
2026-04-06 17:14:37 -0700

    fix: use hashrouter for static hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout.tsx'
 import Home from './pages/Home.tsx'
 import Rules from './pages/Rules.tsx'
@@ -7,7 +7,7 @@ import QuickPlay from './pages/QuickPlay.tsx'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
@@ -16,6 +16,6 @@ export default function App() {
           <Route path="/quickplay" element={<QuickPlay />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
